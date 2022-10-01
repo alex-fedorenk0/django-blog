3edf95c3333a863f2f9880b267740c501ebf2dc9
alex-fedorenk0 <alex-fedorenk0>
2022-10-01 13:29:14 +0300

    added commenting

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 class PostListView(ListView):
     """
@@ -41,10 +41,16 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
     )
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
     return render(
         request,
         'blog/post/detail.xhtml',
-        {'post': post}
+        {
+            'post': post,
+            'form': form,
+            'comments': comments,
+        }
     )
 
 def post_share(request, post_id):
@@ -68,3 +74,26 @@ def post_share(request, post_id):
         request,
         'blog/post/share.xhtml',
         {'post':post, 'form':form, 'sent': sent})
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED,
+    )
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.xhtml',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment,
+        }
+    )
